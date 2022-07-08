6c5e7e7efed0a416db29d4b01520e467d1c06e15
proj_cardapios <cardapio.projeto@gmail.com>
2022-07-08 11:02:16 -0300

    commit-inicial

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-container fluid tex-center>
+      <v-row>
+          <v-col class="text-center pt-15">
+              <h1 class="h1">login</h1>
+          </v-col>
+        </v-row>
+        <v-row class="mx-auto elevation 3 pa-15 text-center ">
+          <v-col >
+          <p>UJHDKJWHVJKLWERHVPKEWUFWLJEHBFWLJFBWWVBEJVBEWRJHVBERJHVBERJVB</p>
+        </v-col>
+            <v-col class="pl-15 pr-15">
+              <v-col class="titulo_login">
+              <h1 class="h1">login</h1>
+              </v-col>
+                <v-form>
+                    <v-text-field label="Email" ></v-text-field>
+                    <v-text-field label="Senha" ></v-text-field>
+                    <v-btn>Login</v-btn>
+                    
+                    
+                </v-form>
+            </v-col>
+            
+        </v-row>
+  </v-container>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+<style scoped>
+.titulo_login {
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
