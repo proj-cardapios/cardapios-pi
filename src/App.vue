19ba7191c0190dc26a9d341a50c358ad27fb92b2
proj-cardapios <CARDAPIO.PROJETO@GMAIL.COM>
2022-07-09 10:19:07 -0300

    tela-de-login

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,5 @@
 <template>
-  <v-container fluid tex-center>
-      <v-row>
-          <v-col class="text-center pt-15">
-              <h1 class="h1">login</h1>
-          </v-col>
-        </v-row>
-        <v-row class="mx-auto elevation 3 pa-15 text-center ">
-          <v-col >
-          <p>UJHDKJWHVJKLWERHVPKEWUFWLJEHBFWLJFBWWVBEJVBEWRJHVBERJHVBERJVB</p>
-        </v-col>
-            <v-col class="pl-15 pr-15">
-              <v-col class="titulo_login">
-              <h1 class="h1">login</h1>
-              </v-col>
-                <v-form>
-                    <v-text-field label="Email" ></v-text-field>
-                    <v-text-field label="Senha" ></v-text-field>
-                    <v-btn>Login</v-btn>
-                    
-                    
-                </v-form>
-            </v-col>
-            
-        </v-row>
-  </v-container>
+  <router-view></router-view>
 </template>
 
 <script>
@@ -37,7 +13,4 @@ export default {
 };
 </script>
 <style scoped>
-.titulo_login {
-  text-align: center;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+
+    <HelloWorld />
 </template>
 
 <script>
@@ -16,3 +14,5 @@ export default {
   }
 }
 </script>
+<style scoped>
+</style>
