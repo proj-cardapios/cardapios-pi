c28c211964d90a50fae00cffbe51f2693f4ab4a1
proj-cardapios <cardapio.projeto@gmail.com>
2022-07-15 00:08:12 -0300

    login quase final

---
+++ src/components/Formcad.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            label="First name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            label="Last name"
+            required
+          ></v-text-field>
+          <v-text-field
+            label="Last name"
+            required
+          ></v-text-field>
+          <v-text-field
+            label="Last name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
