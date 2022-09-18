70e7db90eeb492cc65305512303726dfc60a8d91
cardapios-pi <projeto.cardapios@gmail.com>
2022-09-18 16:58:31 -0300

    Criação de Perfil

--- src/views/Perfil.vue
+++ src/views/Perfil.vue
@@ -1,10 +1,34 @@
 <template>
-<div></div>
+<div>
+    <v-container class="body" fluid>
+    <h2>Meu perfil</h2>
+    <v-form>
+      <v-container>
+      <v-text-field label="Nome"  ></v-text-field>
+      <v-text-field label="Email"  ></v-text-field>
+      <h3>Informações Nutricionais</h3>
+      <v-text-field label="Idade"  ></v-text-field>
+      <v-text-field label="Gênero"  ></v-text-field>
+      <v-text-field label="Peso"  ></v-text-field>
+      <v-text-field label="Altura"  ></v-text-field>
+        <v-btn color="#4DC3C8" @click="Editarperfil">Editar Conta</v-btn>
+        <v-btn color="#B2DFE1" @click="Desconectar">Desconectar Conta</v-btn>  
+      </v-container>
+    </v-form>
+  </v-container>
+</div>
 </template>
 
 <script>
 export default {
-
+    methods: {
+        Editarperfil() {
+            this.$router.push({ name: "Cadastro" });
+        },
+        Desconectar() {
+            this.$router.push({ name: "Login" });
+        },
+    }
 }
 </script>
 
