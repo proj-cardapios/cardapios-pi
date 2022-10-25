2c50633f7e88898610d3eccca166c1a940ab33d5
proj-cardapios <cardapio.projeto@gmail.com>
2022-10-25 16:51:13 -0300

    arrumando o perfil e add o imc

--- src/components/PrfilCard.vue
+++ src/components/PrfilCard.vue
@@ -36,29 +36,36 @@
         <v-card-subtitle class="editcont">
           <v-btn color="#4DC3C8" @click="Editarperfil">Editar Conta</v-btn>
         </v-card-subtitle>
+        <v-card-subtitile>
+          <v-btn color="#B2DFE1" @click="Desconectar">Desconectar Conta</v-btn>
+        </v-card-subtitile>
       </v-col>
       <v-col cols="6">
         <v-row>
-          <v-col cols="8">
+          <v-col cols="11">
             <v-divider color="#B2DFE1"></v-divider>
             <v-card-subtitle>
-              <h3></h3>
+              <h2>Calcule seu IMC</h2>
+              <h3>
+                Com base nos dados do Usuario, o sistema calcula de forma
+                automatica o deu indice de massa corporal
+              </h3>
             </v-card-subtitle>
 
             <v-divider color="#B2DFE1"></v-divider>
-            <v-card-subtitile>
-              <v-btn color="#B2DFE1" @click="Desconectar"
-                >Desconectar Conta</v-btn
-              > 
-            </v-card-subtitile>
-            <v-btn color="#B2DFE1" @click="Desconectar"
-              >Desconectar Conta</v-btn
-            >
-          </v-col>
-          <v-col cols="4">
-            <v-btn color="#B2DFE1" @click="Desconectar"
-              >Desconectar Conta</v-btn
-            >
+            <v-row class="IMCcalc">
+              <v-col cols="3">
+                <v-btn color="#4DC3C8" class="btncalc">Calcular</v-btn>
+                
+              </v-col>
+              <v-col cols="9">
+                <v-divider color="#B2DFE1"></v-divider>
+                <v-card-subtitle>
+                  <imcresultado></imcresultado>
+                </v-card-subtitle>
+                <v-divider color="#B2DFE1"></v-divider>
+              </v-col>
+            </v-row>
           </v-col>
         </v-row>
       </v-col>
@@ -67,13 +74,21 @@
 </template>
 
 <script>
+import imcresultado from "./IMC_resultado.vue";
 export default {
+  components: { imcresultado },
   props: ["Usuario"],
 };
 </script>
 
 <style scoped>
 .editcont {
-  margin: 16px 0 0 0;
+  margin: 1px 0 0 0;
+}
+.IMCcalc {
+  margin: 1px 0 0 0;
+}
+.btncalc {
+  margin: 1rem 0 0 0;
 }
 </style>
--- src/views/Cardapio1.vue
+++ src/views/Cardapio1.vue
@@ -6,10 +6,11 @@
         <v-card shaped>
           <div>
             <v-card color="#B2DFE1">
-              <div><v-card-subtitle>
-                <h2>Nome do Cardápio</h2>
-              </v-card-subtitle></div>
-              
+              <div>
+                <v-card-subtitle>
+                  <h2>Nome do Cardápio</h2>
+                </v-card-subtitle>
+              </div>
             </v-card>
 
             <v-divider></v-divider>
@@ -17,11 +18,12 @@
               <h4>morte a dieta</h4>
             </v-card-subtitle>
             <v-divider color="#4DC3C8"></v-divider>
-            <v-card><div>
-              <v-card-subtitle>
-                
-                <h3>Calorias Totais</h3>
-              </v-card-subtitle></div>
+            <v-card
+              ><div>
+                <v-card-subtitle>
+                  <h3>Calorias Totais</h3>
+                </v-card-subtitle>
+              </div>
             </v-card>
             <v-divider></v-divider>
             <v-card-subtitle>
@@ -122,8 +124,8 @@
                 </v-row>
               </v-expansion-panel-header>
               <v-expansion-panel-content>
-              Para adicionar clique na opção "editar"
-            </v-expansion-panel-content>
+                Para adicionar clique na opção "editar"
+              </v-expansion-panel-content>
             </v-expansion-panel>
           </v-expansion-panels>
           <v-expansion-panels>
@@ -156,35 +158,29 @@
                 </v-row>
               </v-expansion-panel-header>
               <v-expansion-panel-content>
-              <v-row>
-                <v-col cols="4">
-                  <ul><li>Opera</li></ul>
-                  <ul><li>Opera</li></ul>
-                  <ul><li>Opera</li></ul>
-                </v-col>
-                <v-col cols="2">
-                  sdagsgsgsg
-                  djhsdkjhsdçghsdh
-                  pdsjghslkdjgh
-                </v-col>
-                <v-col cols="3">
-                  sdaggsdg
-                  çdkljgslkgj
-                  sdgkjdslgkjdsgçlsdkjgls
-                </v-col>
-                <v-col cols="3">
-                  sdgdsgsdgsdgsdg
-                  sdkçjghsdkgjhsd
-                  glshjgdçlskdjhg
-                </v-col>
-                <ul>
-        <li>Internet Explorer</li>
-	<li>Opera</li>
-	<li>Firefox</li>
-	<li>Safari</li>
-</ul>
-              </v-row>
-            </v-expansion-panel-content>
+                <v-row>
+                  <v-col cols="4">
+                    <ul>
+                      <li>Opera</li>
+                    </ul>
+                    <ul>
+                      <li>Opera</li>
+                    </ul>
+                    <ul>
+                      <li>Opera</li>
+                    </ul>
+                  </v-col>
+                  <v-col cols="2">
+                    sdagsgsgsg djhsdkjhsdçghsdh pdsjghslkdjgh
+                  </v-col>
+                  <v-col cols="3">
+                    sdaggsdg çdkljgslkgj sdgkjdslgkjdsgçlsdkjgls
+                  </v-col>
+                  <v-col cols="3">
+                    sdgdsgsdgsdgsdg sdkçjghsdkgjhsd glshjgdçlskdjhg
+                  </v-col>
+                </v-row>
+              </v-expansion-panel-content>
             </v-expansion-panel>
           </v-expansion-panels>
         </div>
@@ -238,7 +234,6 @@
 </template>
 
 <script>
-
 import Alimento from "../components/Alimento.vue";
 export default {
   props: ["titulo"],
@@ -353,5 +348,4 @@ export default {
   padding: 0;
   text-align: center;
 }
-
 </style>
